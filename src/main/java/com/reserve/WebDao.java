7c92cf27fb581c5894539c056be8a1c80b0ecd51
SeigoKondoh <3th76sk.1156@gmail.com>
2023-12-04 22:27:32 +0900

    DataClassRowMapperへの書き換え

--- src/main/java/com/reserve/WebDao.java
+++ src/main/java/com/reserve/WebDao.java
@@ -3,14 +3,12 @@ package com.reserve;
 import com.reserve.WebController.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.IncorrectResultSizeDataAccessException;
+import org.springframework.jdbc.core.DataClassRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
 
-import java.sql.Date;
-import java.sql.Time;
 import java.time.LocalDate;
 import java.util.List;
-import java.util.Map;
 
 @Service
 public class WebDao {
@@ -28,31 +26,16 @@ public class WebDao {
                 reserve.email(), reserve.tel());
     }
 
-    //        同じ日時で予約されているかを確認
     public List<ReserveDate> search(LocalDate date) {
         String sql = "SELECT * FROM RESERVE WHERE date = ?";
-        List<Map<String, Object>> result = jdbcTemplate.queryForList(sql, date);
-        List<ReserveDate> reserveDates = result.stream()
-                .map((Map<String, Object> row) -> new ReserveDate(
-                        ((Date) row.get("date")).toLocalDate(),
-                        ((Time) row.get("time")).toLocalTime()))
-                .toList();
+        List<ReserveDate> reserveDates = jdbcTemplate.query(sql,new DataClassRowMapper<>(ReserveDate.class),date);
         return reserveDates;
     }
 
-    //    IDで予約情報を確認
     public Reserve idFind(String id) {
         String sql = "SELECT * FROM reserve WHERE id = ?";
         try {
-            Map<String, Object> map = jdbcTemplate.queryForMap(sql, id);
-            Reserve reserve = new Reserve(
-                    map.get("id").toString(),
-                    ((Date) (map.get("date"))).toLocalDate(),
-                    ((Time) map.get("time")).toLocalTime(),
-                    map.get("menu").toString(),
-                    map.get("name").toString(),
-                    map.get("email").toString(),
-                    map.get("tel").toString());
+            Reserve reserve = jdbcTemplate.queryForObject(sql,new DataClassRowMapper<>(Reserve.class),id);
             return reserve;
         } catch (IncorrectResultSizeDataAccessException e) {
             return null;
